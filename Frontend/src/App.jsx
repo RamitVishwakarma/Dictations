0bfc5d0ccdba24d404bad20b474c72cc9cc8aa35
RamitVishwakarma <vishwakarmaramit@gmail.com>
2024-02-27 23:34:05 +0530

    Fixed an issue with text box

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,23 @@
 import Navbar from "./components/Navbar";
 import image from "./assets/Images/Radial-Gradient.png";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import axios from "axios";
 
 export default function App() {
-  const [text, setText] = useState("Hello World!");
+  const text = "Enter your text here";
   const [delay, setDelay] = useState();
+  const textRef = useRef(null);
   const handleDelay = (e) => {
     setDelay(e.target.value);
   };
 
-  const handleText = (e) => {
-    setText(e.currentTarget.textContent);
-  };
-
   const buttonClickHandler = () => {
+    const text = textRef.current.innerText;
+    console.log(text);
+    if (!text) {
+      alert("Please enter some text");
+      return;
+    }
     if (!delay || isNaN(Number(delay))) {
       alert("Please enter a valid delay value");
       return;
@@ -43,19 +46,18 @@ export default function App() {
         <div className="mx-10">
           <Navbar />
           <img className="absolute bottom-0 z-1" src={image} />
-          {/* The input area */}
           <div className="mt-10 flex flex-col items-center justify-center gap-10 z-10">
             <input
               className="w-[50vw] p-2 text-xl text-green z-10 bg-[#060f11] rounded-xl shadow-xl  hover:outline hover:outline-green focus:outline focus:outline-green"
               type="text"
               placeholder="Delay in ms"
               value={delay}
-              onChange={handleDelay}
+              onInput={handleDelay}
             />
             <div
               className="text-4xl custom-scrollbar text-green font-gugi z-10 min-w-[50vw] rounded-xl p-2 min-h-[50vh] max-w-[60vw] max-h-[55vh] outline-none hover:outline hover:outline-green focus:outline focus:outline-green shadow-2xl overflow-y-scroll"
               contentEditable="true"
-              onInput={handleText}>
+              ref={textRef}>
               {text}
             </div>
             <button
